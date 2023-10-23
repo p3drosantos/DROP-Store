55b00ca3347ff6c2497590e3622c37ed90d219f1
Pedro Santos <P3dro.santosc@gmail.com>
2023-10-23 19:46:39 -0300

    feat: add auth handler to header component

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   HomeIcon,
   ListOrderedIcon,
@@ -9,8 +10,12 @@ import {
 import { Button } from "./button";
 import { Card } from "./card";
 import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
-
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
+import { Separator } from "./separator";
 const Header = () => {
+  const { status, data } = useSession();
+
   return (
     <Card className="flex justify-between p-[1.875rem]">
       <Sheet>
@@ -25,11 +30,48 @@ const Header = () => {
             Menu
           </SheetHeader>
 
-          <div className="mt-2 flex flex-col gap-2">
-            <Button variant="outline" className="w-full justify-start gap-2">
-              <LogInIcon size={16} />
-              Fazer Login
-            </Button>
+          {status === "authenticated" && data?.user && (
+            <div className="flex flex-col">
+              <div className="flex items-center gap-2 py-4">
+                <Avatar>
+                  <AvatarFallback>
+                    {data.user.name?.[0].toUpperCase()}
+                  </AvatarFallback>
+                  {data.user.image && <AvatarImage src={data.user.image} />}
+                </Avatar>
+
+                <div className="flex flex-col">
+                  <p className="text-sm font-medium">{data.user.name}</p>
+                  <p className=" text-xs opacity-75">Boas Compras!</p>
+                </div>
+              </div>
+              <Separator />
+            </div>
+          )}
+
+          <div className="mt-4 flex flex-col gap-2">
+            {status === "unauthenticated" && (
+              <Button
+                variant="outline"
+                className="w-full justify-start gap-2"
+                onClick={() => signIn()}
+              >
+                <LogInIcon size={16} />
+                Entrar
+              </Button>
+            )}
+
+            {status === "authenticated" && (
+              <Button
+                variant="outline"
+                className="w-full justify-start gap-2"
+                onClick={() => signOut()}
+              >
+                <LogInIcon size={16} />
+                Sair
+              </Button>
+            )}
+
             <Button variant="outline" className="w-full justify-start gap-2">
               <HomeIcon size={16} />
               InÍcio
