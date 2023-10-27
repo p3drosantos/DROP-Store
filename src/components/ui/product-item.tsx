878470d3550a7b1231a36d63c6038d117f01b8fc
Pedro Santos <P3dro.santosc@gmail.com>
2023-10-27 20:54:53 -0300

    fix: ensure product prices are wrapped in one line

--- src/components/ui/product-item.tsx
+++ src/components/ui/product-item.tsx
@@ -34,7 +34,7 @@ const ProductItem = ({ product }: ProductItemProps) => {
           {product.discountPercentage > 0 ? (
             <>
               {" "}
-              <p className="font-semibold">
+              <p className=" text-sm font-semibold">
                 R$ {product.totalPrice.toFixed(2)}
               </p>
               <p className="text-xs line-through opacity-75">
@@ -42,7 +42,9 @@ const ProductItem = ({ product }: ProductItemProps) => {
               </p>
             </>
           ) : (
-            <p className="font-semibold">R$ {product.basePrice.toFixed(2)}</p>
+            <p className="text-sm font-semibold">
+              R$ {product.basePrice.toFixed(2)}
+            </p>
           )}
         </div>
       </div>
