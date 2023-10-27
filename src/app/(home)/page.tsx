cd010783b44419b647f857e6a95c563dccea38bc
Pedro Santos <P3dro.santosc@gmail.com>
2023-10-27 20:38:16 -0300

    feat: add mouses section to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -21,17 +21,25 @@ export default async function Home() {
     },
   });
 
+  const mouses = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: "mouses",
+      },
+    },
+  });
+
   return (
-    <div>
+    <div className="flex flex-col gap-8">
       <PromoBanner
         src="/banner-home-01.png"
         alt="Até 55% de desconto esse mês"
       />
 
-      <div className="mt-8 px-5">
+      <div className=" px-5">
         <Categories />
       </div>
-      <div className=" mt-8">
+      <div>
         <SectionTitle className="mb-2 pl-5 font-bold uppercase">
           Ofertas
         </SectionTitle>
@@ -43,11 +51,25 @@ export default async function Home() {
         alt="Até 55% de desconto em mouses"
       />
 
-      <div className=" mt-8">
+      <div>
         <SectionTitle className="mb-2 pl-5 font-bold uppercase">
-          teclados
+          Teclados
         </SectionTitle>
         <ProductList products={keyboards} />
+      </div>
+
+      <div>
+        <PromoBanner
+          src="/banner-home-03.png"
+          alt="Até 55% de desconto em mouses"
+        />
+      </div>
+
+      <div>
+        <SectionTitle className="mb-2 pl-5 font-bold uppercase">
+          Mouses
+        </SectionTitle>
+        <ProductList products={mouses} />
       </div>
     </div>
   );
