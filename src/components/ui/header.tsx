53166a5590ba33e28a0c87bd6420938de7594f25
Pedro Santos <P3dro.santosc@gmail.com>
2023-11-24 20:13:36 -0300

    feat: add deals page

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -92,10 +92,18 @@ const Header = () => {
               </Link>
             </SheetClose>
 
-            <Button variant="outline" className="w-full justify-start gap-2">
-              <Percent size={16} />
-              Ofertas
-            </Button>
+            <SheetClose asChild>
+              <Link href="/deals">
+                <Button
+                  variant="outline"
+                  className="w-full justify-start gap-2"
+                >
+                  <Percent size={16} />
+                  Ofertas
+                </Button>
+              </Link>
+            </SheetClose>
+
             <SheetClose asChild>
               <Link href="/catalog">
                 <Button
