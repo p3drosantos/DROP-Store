f3dd9611848ed3aa18e34b551020d4d619473b7d
Pedro Santos <P3dro.santosc@gmail.com>
2023-12-09 18:03:20 -0300

    style: add hover on category list buttons

--- src/app/(home)/category-item.tsx
+++ src/app/(home)/category-item.tsx
@@ -12,7 +12,7 @@ const CategoryItem = ({ category }: CategoryItemProps) => {
     <Link href={`/category/${category.slug}`}>
       <Badge
         variant="outline"
-        className="flex cursor-pointer items-center justify-center gap-2 rounded-lg py-3"
+        className="flex cursor-pointer items-center justify-center gap-2 rounded-lg py-3  hover:bg-primary hover:bg-opacity-90 hover:text-white"
       >
         {CATEGORY_ICON[category.slug as keyof typeof CATEGORY_ICON]}
         <span className="text-xs font-bold">{category.name}</span>
